LineageOS/android_packages_services_Telephony

Fix an issue wrong network operator name is displayed on MSIM devices
SIM1 operator network name is displayed as SIM2 operator network name on
Mobile Network setting because TelephonyManager#getNetworkOperatorName
with no subId is always used to set the summary text.

To resolve this issue, use TelephonyManager#getNetworkOperatorName with
subId to get operator network name based on SIM.

Bug: 109936826
Test: manual - Checked that the network name is displayed according to
the SIM network operator
Change-Id: Ief6d5148ef0e27bb31d869994cf64ba26ce4f7d4

src/com/android/phone/NetworkSelectListPreference.java
@@ -227,7 +227,7 @@ public class NetworkSelectListPreference extends ListPreference
         TelephonyManager telephonyManager = (TelephonyManager)
                 getContext().getSystemService(Context.TELEPHONY_SERVICE);
 
-        setSummary(telephonyManager.getNetworkOperatorName());
+        setSummary(telephonyManager.getNetworkOperatorName(mSubId));
 
         setOnPreferenceChangeListener(this);
     }

adi8900/platform_packages_services_Telephony

src/com/android/phone/NetworkSelectListPreference.java
@@ -210,7 +210,7 @@ public class NetworkSelectListPreference extends ListPreference
         mNetworkScanHelper = new NetworkScanHelper(
                 mTelephonyManager, mCallback, mNetworkScanExecutor);
 
-        setSummary(mTelephonyManager.getNetworkOperatorName());
+        setSummary(mTelephonyManager.getNetworkOperatorName(mSubId));
 
         setOnPreferenceChangeListener(this);
     }